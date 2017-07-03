e41c48875b62cf8be1bad163cae3260dd1a32d04
William Surya Wijaya <williamsw.gb@gmail.com>
2017-07-04 00:24:35 +0800

    Update package and webpack, set eslint and babel

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,37 @@
+var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+var HTMLWebpackPluginConfig = new HtmlWebpackPlugin({
+  template: __dirname + '/app/html/index.html',
+  filename: 'index.html',
+  inject: 'body'
+});
+
+var plugins = [
+  HTMLWebpackPluginConfig,
+];
+
 module.exports = {
   context: __dirname + "/app",
-
-  entry: "./js/app.js",
-
+  entry: {
+    javascript: "./js/index.js",
+  },
   output: {
-    filename: "app.js",
+    filename: "main.js",
     path: __dirname + "/dist",
-  }
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.json'],
+    root: path.resolve(__dirname, './app/js'),
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: '/node_modules/',
+        loader: 'babel-loader',
+      },
+    ]
+  },
+  plugins: plugins,
 };
